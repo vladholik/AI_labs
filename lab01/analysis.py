aa67dd2f0de59ef93b53b4963751f46042d4cc9b
vladholik <71918388+vladholik@users.noreply.github.com>
2025-06-05 16:42:33 +0300

    smth stable

--- lab01/analysis.py
+++
@@ -1,9 +0,0 @@
-import  pandas as pd
-
-df = pd.read_csv("train.csv")
-
-print("5 первых строк датасета")
-print(df.head(), "\n")
-
-print("Пропущенные значения по столбцам")
-print(df.isnull().sum())
